d5ec67c9a0adbcc81e0956eef579a3ca133b6dc2
YarodAB <yarod.aliaga@gmail.com>
2023-11-06 19:38:19 -0300

    Se hacen las funcionalidades de la vista para la carga de datos, ahora se abre la pantalla de carga de datos desde la pantalla principal. Se crea la clase mascota y dueño para su posterior conexión a BD

---
+++ PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/domain/Mascota.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.peluqueriacanina.domain;
+
+/**
+ *
+ * @author Yarod
+ */
+public class Mascota {
+    private int numCliente;
+    private String nombre;
+    private String raza;
+    private String color;
+    private String alergico;
+    private String atencionEspecial;
+    private String Observaciones;
+    
+    private Dueno dueno;
+
+    public Mascota() {
+    }
+
+    public Mascota(int numCliente, String nombre, String raza, String color, String alergico, String atencionEspecial, String Observaciones, Dueno dueno) {
+        this.numCliente = numCliente;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.alergico = alergico;
+        this.atencionEspecial = atencionEspecial;
+        this.Observaciones = Observaciones;
+        this.dueno = dueno;
+    }
+
+    public int getNumCliente() {
+        return numCliente;
+    }
+
+    public void setNumCliente(int numCliente) {
+        this.numCliente = numCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAlergico() {
+        return alergico;
+    }
+
+    public void setAlergico(String alergico) {
+        this.alergico = alergico;
+    }
+
+    public String getAtencionEspecial() {
+        return atencionEspecial;
+    }
+
+    public void setAtencionEspecial(String atencionEspecial) {
+        this.atencionEspecial = atencionEspecial;
+    }
+
+    public String getObservaciones() {
+        return Observaciones;
+    }
+
+    public void setObservaciones(String Observaciones) {
+        this.Observaciones = Observaciones;
+    }
+
+    public Dueno getDueno() {
+        return dueno;
+    }
+
+    public void setDueno(Dueno dueno) {
+        this.dueno = dueno;
+    }
+    
+    
+    
+}
