a5ffb1a30a1a886f61ae3e5ca20b810e00286cb6
YarodAB <yarod.aliaga@gmail.com>
2023-11-09 16:45:00 -0300

    Se crea la logica de conexión entre la persistencia y la logica a través de instancias en sus respectivas controladoras

---
+++ PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/domain/Controladora.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.peluqueriacanina.domain;
+
+import com.mycompany.peluqueriacanina.persistencia.ControladoraPersistencia;
+
+/**
+ *
+ * @author Yarod
+ */
+public class Controladora {
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+}
